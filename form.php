c4998d627a39ed2015485cbedf78822093139af4
Grigorieva1Dasha <32798782+Grigorieva1Dasha@users.noreply.github.com>
2019-06-27 19:03:53 +0800

    Create form.php

---
+++ form.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Thanks</title>
+	<link href="css/view.css" rel="stylesheet">
+</head>
+<body>
+	<center>
+		<h1>Спасибо, ваша заявка принята!</h1>
+		<p><a href="/ls/form10">Отправить заявку еще раз.</p>
+		<form action="admin.php">
+        	<p><input type="submit" value="Администратор" class="button1"></p>
+    	</form>
+	</center>
+</body>
+</html>
